02003c5ba90cbfb68e59b51ebd58f84e650085de
Dante Hernandez <dantehernandez587@gmail.com>
2025-10-28 08:58:25 -0400

    I did it

---
+++ today.js
@@ -0,0 +1,22 @@
+// how to write a for loop
+for( let i = 0; i < 10; i++){
+    console.log("Today is a great day!");
+}
+
+// Java Array
+// int [] myArray = {2,4,6}
+let myArray = [2,4,6];
+console.log(myArray); // 4
+
+// index starts at 0
+// last index = length - 1
+// length = 3, last index = 2
+
+for(let i = 0; i < myArray.length; i++){
+    console.log(myArray[i]*2);
+}
+
+let array2 =[1,2,3];
+for(let i = 0; i<array2.length; i++){
+    
+}
